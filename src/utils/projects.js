056502be4da338c51f0a8e98a6529b6e34f049d3
wamu <101846564+tkzero789@users.noreply.github.com>
2024-10-25 17:37:04 -0400

    feat: update Project Insight page

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -4,9 +4,30 @@ import Dime from "../assets/images/Dime/dime-project.webp";
 
 const data = [
   {
+    name: "Dime",
+    type: "Website",
+    href: "/projects/dime",
+    url: "https://dime-mu.vercel.app/",
+    github: "https://github.com/tkzero789/dime",
+    image: Dime,
+    slug: "dime",
+    description:
+      "Budgeting app helps users manage their finances effortlessly. Track spending, set goals, and get personalized financial tips from an AI assistant to improve money management skills.",
+    tech: [
+      "Next.js",
+      "Clerk.js",
+      "Drizzle ORM",
+      "PostgreSQL",
+      "OpenAI",
+      "Tailwind CSS",
+      "shadcn/ui",
+    ],
+  },
+  {
     name: "BaySide Hospital",
     type: "Website",
     url: "https://bayside-hospital.onrender.com/home",
+    href: "/projects/bayside",
     github: "https://github.com/tkzero789/Hospital",
     image: BaySide,
     slug: "bayside-hospital",
@@ -17,39 +38,22 @@ const data = [
       "Node.js",
       "Express.js",
       "MongoDB",
-      "SCSS",
+      "Sass",
       "Bootstrap",
       "MUI",
     ],
   },
   {
-    name: "Dime",
-    type: "Website",
-    url: "https://dime-mu.vercel.app/",
-    github: "https://github.com/tkzero789/dime",
-    image: Dime,
-    slug: "dime",
-    description:
-      "Budgeting app helps users manage their finances effortlessly. Track spending, set goals, and get personalized financial tips from an AI assistant to improve money management skills.",
-    tech: [
-      "Next.js",
-      "Clerk.js",
-      "Neon",
-      "OpenAI",
-      "Tailwind CSS",
-      "shadcn/ui",
-    ],
-  },
-  {
     name: "PocketSky",
     type: "Website",
+    href: "/projects/pocketsky",
     url: "https://main--brilliant-klepon-275efd.netlify.app/",
     github: "https://github.com/tkzero789/PocketSky",
     image: PocketSKy,
     slug: "pocket-sky",
     description:
       "A simple weather app with search features, unit conversion, and essential forecast information.",
-    tech: ["HTML", "CSS", "Javascript"],
+    tech: ["HTML", "CSS", "JavaScript"],
   },
 ];
 
